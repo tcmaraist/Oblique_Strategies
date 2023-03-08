0af226411febb195812a9f370acce48a87726665
Ty Maraist <tcmaraist@gmail.com>
2023-03-08 10:49:41 -0600

    create getStrategy function to display data. center objects on screen

---
+++ index.js
@@ -0,0 +1,16 @@
+function getStrategy() {
+  fetch('./strategies.json')
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      let random =
+        data.Strategies[Math.floor(Math.random() * data.Strategies.length)];
+      quotes.innerText = `"${random}."`;
+      console.log(random);
+    });
+}
+
+getStrategy();
+
+document.querySelector('button').addEventListener('click', getStrategy);
